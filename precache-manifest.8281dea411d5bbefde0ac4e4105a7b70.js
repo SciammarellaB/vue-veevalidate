d361ac69974e11318b409ceedffa008d2846f535
BrennoSciammarella <brenno.s97@gmail.com>
2022-01-28 22:17:33 -0300

    Deploy

---
+++ precache-manifest.8281dea411d5bbefde0ac4e4105a7b70.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f02dd627f1181e267a5",
+    "url": "/vue-veevalidate/css/app.2a378f90.css"
+  },
+  {
+    "revision": "62926b8b25bbc695a355",
+    "url": "/vue-veevalidate/css/chunk-c91ab20a.3185ede8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-veevalidate/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a5da8e28beffcddbf1f504abaa0502ce",
+    "url": "/vue-veevalidate/index.html"
+  },
+  {
+    "revision": "3f02dd627f1181e267a5",
+    "url": "/vue-veevalidate/js/app.094fa3ca.js"
+  },
+  {
+    "revision": "62926b8b25bbc695a355",
+    "url": "/vue-veevalidate/js/chunk-c91ab20a.7071af27.js"
+  },
+  {
+    "revision": "4b9c88c0938ee2bc3fe5",
+    "url": "/vue-veevalidate/js/chunk-vendors.1d720d6b.js"
+  },
+  {
+    "revision": "9adcc6be7eadbeb29f7233e846966413",
+    "url": "/vue-veevalidate/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-veevalidate/robots.txt"
+  }
+]);
